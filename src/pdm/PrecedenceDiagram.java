9e81e6c9f00c11ebda7650012ae0a7b441cdf42e
Alex Lende <ajlende@iastate.edu>
2015-10-15 12:17:47 -0500

    method stubs and beginning of pdm class

--- src/pdm/PrecedenceDiagram.java
+++ src/pdm/PrecedenceDiagram.java
@@ -1,9 +1,73 @@
 package pdm;
 
+import java.io.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class PrecedenceDiagram {
 
-	public PrecedenceDiagram() {
-		// TODO Auto-generated constructor stub
+	private Set<Task> tasks;
+
+	public PrecedenceDiagram(Set<Task> tasks) {
+		this.tasks = tasks;
 	}
 
+	public PrecedenceDiagram(String filename) {
+		tasks = new HashSet<>();
+		this.readTasks(filename);
+	}
+
+	public void addTask(Task task) {
+		this.tasks.add(task);
+	}
+
+	public void removeTask(Task task) {
+		this.tasks.remove(task);
+	}
+
+	public Task findTask(String name) {
+		return null;
+	}
+
+	public void readTasks(String csvFile) {
+		BufferedReader br = null;
+		String line;
+		String cvsSplitBy = ",";
+
+		try {
+
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+
+				// use comma as separator
+				String[] taskFields = line.split(cvsSplitBy);
+				List<Task> dependancies = null;
+				Task task = new Task(taskFields[0], taskFields[1], dependancies);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public Set<Task> getCriticalPaths() {
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
